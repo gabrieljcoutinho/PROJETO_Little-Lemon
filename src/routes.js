edd6f39392e77c9e0ab62d23c018298d141d0d55
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-24 09:43:50 -0300

    nova importação

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Header from './ts/Header.tsx'
+import MainConteudoPrincipal from "./ts/MainConteudoPrincipal.tsx"
 
 const routes = () => {
   return (
     <div>
 
             <Header />
+
+                 <MainConteudoPrincipal />
 
 
     </div>
