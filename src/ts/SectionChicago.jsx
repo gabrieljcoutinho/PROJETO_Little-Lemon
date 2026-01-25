2ec51a4b84a14e11633a1bb7cad27c6a38204eed
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-25 10:16:42 -0300

    novo componete

---
+++ src/ts/SectionChicago.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/sectionChicago.css"
+
+type Props = {}
+
+const SectionChicago = (props: Props) => {
+  return (
+    <div>SectionChicago</div>
+  )
+}
+
+export default SectionChicago
