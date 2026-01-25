c5b1c095f2b3e33798557c28055649972e481ede
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-24 23:13:54 -0300

    imgs

--- src/ts/SectionPratos.jsx
+++ src/ts/SectionPratos.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "../css/sectionPratos.css"
 
+import prato1 from "../../src/imgs/foodSpecial1.png"
+import prato2 from "../../src/imgs/foodSpecial2.png"
+import prato3 from "../../src/imgs/foodSpecial3.png"
+
 type Props = {}
 
 const SectionPratos = (props: Props) => {
@@ -19,12 +23,15 @@ const SectionPratos = (props: Props) => {
             <div className="flexbox">
 
             <div class="div1">
+              <img src={prato1} alt="" />
             </div>
 
             <div class="div1">
+               <img src={prato2} alt="" />
             </div>
 
             <div class="div1">
+               <img src={prato3} alt="" />
             </div>
 
             </div>
