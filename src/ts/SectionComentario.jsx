00e8adacd2379c9d138ed90483e50a85e020b54a
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-25 00:03:55 -0300

    melhorias

--- src/ts/SectionComentario.jsx
+++ src/ts/SectionComentario.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "../css/comentarios.css"
 
+import estrela from "../imgs/star.png"
+
 type Props = {}
 
 const SectionComentario = (props: Props) => {
@@ -12,20 +14,50 @@ const SectionComentario = (props: Props) => {
             <div className="flex">
 
             <div className="comentarios">
-      Comentario
+                  <ul className="listaEstrelas">
+                    <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+              <img src={estrela} alt=""  className='imgEstrela'/>
+                  </ul>
             </div>
 
             <div className="comentarios">
-      Comentario
+              <div className="comentarios">
+                  <ul className="listaEstrelas">
+                    <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+              <img src={estrela} alt=""  className='imgEstrela'/>
+                  </ul>
+            </div>
             </div>
 
             <div className="comentarios">
-      Comentario
+        <div className="comentarios">
+                  <ul className="listaEstrelas">
+                    <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+              <img src={estrela} alt=""  className='imgEstrela'/>
+                  </ul>
+            </div>
             </div>
 
 
             <div className="comentarios">
-      Comentario
+      <div className="comentarios">
+                  <ul className="listaEstrelas">
+                    <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+            <img src={estrela} alt=""  className='imgEstrela'/>
+              <img src={estrela} alt=""  className='imgEstrela'/>
+                  </ul>
+            </div>
             </div>
 
             </div>
