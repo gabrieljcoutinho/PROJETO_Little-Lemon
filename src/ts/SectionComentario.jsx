5cb3cb04c45ea54a2b4908c656dc49d584752534
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-24 23:41:02 -0300

    noco css e rotas

---
+++ src/ts/SectionComentario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/comentarios.css"
+
+type Props = {}
+
+const SectionComentario = (props: Props) => {
+  return (
+    <div>SectionComentaasdrio</div>
+  )
+}
+
+export default SectionComentario
