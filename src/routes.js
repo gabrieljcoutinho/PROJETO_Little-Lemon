9ccb0d41febe10209748fc409e051d97956290ec
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-25 10:16:47 -0300

    novo componete

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Header from './ts/Header.tsx'
 import MainConteudoPrincipal from "./ts/MainConteudoPrincipal.tsx"
 import SectionPratos from "./ts/SectionPratos.jsx"
 import SectionComentarios from "./ts/SectionComentario.jsx"
+import SectionChicago from "./ts/SectionChicago.jsx"
 
 const routes = () => {
   return (
@@ -18,6 +19,8 @@ const routes = () => {
                  <SectionPratos />
 
         <SectionComentarios />
+
+        <SectionChicago />
 
     </div>
   )
