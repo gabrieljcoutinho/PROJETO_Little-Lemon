d0a2560248b106e67f8ba4593e5fecb46fd7380c
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-25 10:48:48 -0300

    footer

---
+++ src/ts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/footer.css"
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div className='footer'>Footer</div>
+  )
+}
+
+export default Footer
